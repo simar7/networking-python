d7fc55aabec915fecd6b8bd9fb308274642c65db
Simarpreet Singh <s244sing@uwaterloo.ca>
2014-10-06 23:45:26 -0400

    tester: adding variable test run timer
    Signed-off-by: Simarpreet Singh <s244sing@uwaterloo.ca>

--- project1/tester.py
+++ project1/tester.py
@@ -11,18 +11,20 @@ import subprocess
 
 rhoValues = 0
 testList = []
+times = 1
 
 # TODO: We can do another argparse here but whatever.
 def main(args):
     if args[1] == 'Q2':
-        times = 1
+        global times
+        times = int(args[2])
         testList.append('./simulator.py --generation M --tickLen 0.0000001 --numOfTicks 1000000 --service D --lambda 100 -L 2000 -C 1000000 --size inf')
 
     elif args[1] == 'Q3' and args[2] is not None:
         # valid start and end are 0.3 and 0.9
         start = float(args[2])
         end = float(args[3])
-        times = 1
+        times = int(args[4])
 
         global rhoValues
         rhoValues = numpy.arange(start, end, 0.1)
